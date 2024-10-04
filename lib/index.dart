68a450578d707fbc51ecd3ecc0008cb4eca17f61
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-04 14:42:51 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/index.dart
@@ -0,0 +1,4 @@
+// Export pages
+export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
+export '/general/initial/initial_widget.dart' show InitialWidget;
+export '/auth1/auth1_widget.dart' show Auth1Widget;
